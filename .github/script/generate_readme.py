5a87b5e0d01a32c9c966b411f60775d417c93f20
moonD4rk <moond4rk13@gmail.com>
2024-01-27 17:57:03 +0800

    refactor: Refactor code to use constants and simplify functions
    - Refactor the `.github/script/generate_readme.py` file for readability and efficiency
    - Import the `requests` library and use the `requests.Session()` object instead of importing functions
    - Simplify the `fetch_all_movies` and `new_img_tag` functions using list comprehensions and f-strings, respectively
    - Create a new method `generate_cover_url` to generate the cover image URL and a new method `fetch_image_url_with_ajax` to replace `image_url_with_ajax`
    - Modify the `is_url_exist` and `new_img_tag` functions to take strings as arguments instead of tuples
    - Replace some hardcoded strings with constants in the `Constants` class
    - Update `print` statements to reflect the operation being performed

--- .github/script/generate_readme.py
+++ .github/script/generate_readme.py
@@ -1,101 +1,88 @@
 import re
+import requests
 from dataclasses import dataclass
 from random import SystemRandom
-
-from requests import get as req_get
-from requests import head as req_head
 from bs4 import BeautifulSoup
 
 
 @dataclass(frozen=True, slots=True)
 class Constants:
-    """
-    MovieCoverConstants
-    -------------
-    """
+    def __init__(self):
+        pass
+
     main_url: str = "https://letterboxd.com/moonD4rk/films/"
     size_in_path: str = '-0-460-0-690-crop'
     name_split_pattern = re.compile(r"[\w']+")
     start_comment: str = '<!--START_SECTION:movie_cover-->'
     end_comment: str = '<!--END_SECTION:movie_cover-->'
     block_pattern: str = f'{start_comment}[\\s\\S]+{end_comment}'
+    readme_path: str = "../../README.md"
+    img_template: str = "https://a.ltrbxd.com/resized/film-poster/{id}-{name}{size}.jpg"
+    ajax_template: str = "https://letterboxd.com/ajax/poster{link}std/70x105/"
+
+
+session = requests.Session()
 
 
 def fetch_all_movies() -> list[tuple[str, str, str]]:
-    """
-    return image url, and image title.
-    """
-    r = req_get(consts.main_url)
-    main_content = r.content
-    soup = BeautifulSoup(main_content, features="lxml")
-    div_list = soup.find_all('div', attrs={
-        "class": "film-poster"
-    })
-    m = []
-    for i in div_list:
-        film_id = i.attrs["data-film-id"]
-        ajax_link = i.attrs["data-target-link"]
-        img = i.find("img")
-        film_name = img.attrs["alt"]
-        m.append((film_name, film_id, ajax_link))
-    return m
+    response = session.get(Constants.main_url)
+    response.raise_for_status()
+
+    soup = BeautifulSoup(response.content, features="lxml")
+    div_list = soup.find_all('div', attrs={"class": "film-poster"})
+    return [(img.attrs["alt"], i.attrs["data-film-id"], i.attrs["data-target-link"])
+            for i in div_list if (img := i.find("img"))]
 
 
-def cover_url(index, film_name, film_id):
-    s = "https://a.ltrbxd.com/resized/film-poster/{}-{}{}.jpg"
-    id = "{}/{}".format("/".join(list(film_id)), film_id)
-    name = "-".join(consts.name_split_pattern.findall(film_name)).lower()
-    return index, s.format(id, name, consts.size_in_path)
+def generate_cover_url(film_name: str, film_id: str) -> str:
+    id_path = "/".join(list(film_id)) + f"/{film_id}"
+    name = "-".join(Constants.name_split_pattern.findall(film_name)).lower()
+    return Constants.img_template.format(id=id_path, name=name, size=Constants.size_in_path)
 
 
-def image_url_with_ajax(index: int, target_link: str):
-    s = "https://letterboxd.com/ajax/poster{}std/70x105/".format(target_link)
-    r = req_get(s)
-    soup = BeautifulSoup(r.content, features="lxml")
-    img = soup.find("img", attrs={
-        "class": "image"
-    })
-    link = img.attrs["src"]
-    return index, link.replace("-0-70-0-105-crop", consts.size_in_path)
+def fetch_image_url_with_ajax(target_link: str) -> str:
+    response = session.get(Constants.ajax_template.format(link=target_link))
+    response.raise_for_status()
+
+    soup = BeautifulSoup(response.content, features="lxml")
+    img = soup.find("img", attrs={"class": "image"})
+    if img and (src := img.attrs.get("src")):
+        return src.replace("-0-70-0-105-crop", Constants.size_in_path)
+    raise ValueError("Image source not found in AJAX response")
 
 
-def is_url_exist(url: tuple[int, str]):
-    """
-    :param url:
-    :return:
-    """
-    return req_head(url=url[1]).ok
+def is_url_exist(url: str) -> bool:
+    return session.head(url).ok
 
 
-def new_img(url: str):
+def new_img_tag(url: str) -> str:
     return f'<img align="right" src="{url}" width="242" height="340" alt="letter-movie-cover">'
 
 
-# fetch_image()
 if __name__ == '__main__':
     consts = Constants()
     rand = SystemRandom()
 
-    print("download image...")
-    fetched_movies: list[tuple[str, str, str]] = fetch_all_movies()
-    movie_urls: list[tuple[int, str]] = [cover_url(i, v[0], v[1]) for i, v in enumerate(fetched_movies)]
-    movie_url: tuple[int, str] = rand.choice(movie_urls)
+    print("Downloading image...")
+    movies = fetch_all_movies()
+    movie_urls = [(i, generate_cover_url(name, film_id)) for i, (name, film_id, _) in enumerate(movies)]
+    movie_url = rand.choice(movie_urls)
 
-    if not is_url_exist(movie_url):
-        movie_url: tuple[int, str] = image_url_with_ajax(movie_url[0], fetched_movies[movie_url[0]][2])
+    if not is_url_exist(movie_url[1]):
+        movie_url = (movie_url[0], fetch_image_url_with_ajax(movies[movie_url[0]][2]))
 
-    print("read old readme...")
-    with open("../../README.md", "r") as old_readme:
+    print("Reading old README...")
+    with open(Constants.readme_path, "r") as old_readme:
         content = old_readme.read()
 
     new_readme = re.sub(
-        pattern=consts.block_pattern,
-        repl=f'{consts.start_comment}\n{new_img(movie_url[1])}\n{consts.end_comment}',
+        pattern=Constants.block_pattern,
+        repl=f'{Constants.start_comment}\n{new_img_tag(movie_url[1])}\n{Constants.end_comment}',
         string=content
     )
 
-    print("write to new readme...")
-    with open("../../README.md", "w+") as f:
+    print("Writing to new README...")
+    with open(Constants.readme_path, "w") as f:
         f.write(new_readme)
 
-    print("finished!")
+    print("Finished!")
